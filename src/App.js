a42593e3e9aea0923457e9b1bec2dc1472a06d10
Riley <riley@officehours.co>
2020-02-29 13:44:53 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-/* global fetch */
-import React, { useState, useEffect } from 'react';
+cd.. /* global fetch */
+import React, { useState, useEffect }
+from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const [message, setMessage] = useState('...loading')
+    const [message, setMessage] = useState('...loading')
 
-  useEffect(() => {
-    async function fetchData () {
-      try {
-        let data = await (await fetch('/api')).json()
-        setMessage(data.message)
-      } catch (err) {
-        setMessage(err.message)
-      }
-    }
-    fetchData()
-  })
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                let data = await (await fetch('/api')).json()
+                setMessage(data.message)
+            } catch (err) {
+                setMessage(err.message)
+            }
+        }
+        fetchData()
+    })
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>Hello, world</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p > { message } < /p> <
+        p > Hello, world < /p> <
+        p >
+        Edit < code > src / App.js < /code> and save to reload. <
+        /p> <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> <
+        /header> <
+        /div>
+    );
 }
 
 export default App;
